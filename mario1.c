f0745de67a7c98417304e2e9ccfc7c84b5715635
Hector Gonzalez <raftb.gonzalez@outlook.com>
2018-11-27 23:07:54 +0000

    Run

---
+++ mario1.c
@@ -0,0 +1,13 @@
+// Example of how to use for loops
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    for(int i = 0; i < 3; i++)
+    {
+        printf("?");
+    }
+    printf("\n");
+}
